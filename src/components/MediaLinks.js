3acd22623177a2a62a2d9824e58673e8e7b1f6f6
dork7 <weeday425@gmail.com>
2022-03-03 14:29:53 +0500

    dataSet

--- src/components/MediaLinks.js
+++ src/components/MediaLinks.js
@@ -12,22 +12,22 @@ const MediaLinks = () => {
   const { links } = useUserDataSet();
   return (
     <HStack alignSelf={"center"} pt={4} spacing={6}>
-      {links.linkedin !== "" && (
+      {links.linkedin && (
         <Link href={links.linkedin} isExternal>
           <Icon as={FaLinkedinIn} w={6} h={6} />
         </Link>
       )}
-      {links.github !== "" && (
+      {links.github && (
         <Link href={links.github} isExternal>
           <Icon as={FaGithub} w={6} h={6} />
         </Link>
       )}
-      {links.facebook !== "" && (
+      {links.facebook && (
         <Link href={links.facebook} isExternal>
           <Icon as={FaFacebookF} w={6} h={6} />
         </Link>
       )}
-      {links.instagram !== "" && (
+      {links.instagram && (
         <Link href={links.instagram} isExternal>
           <Icon as={FaInstagram} w={6} h={6} />
         </Link>
