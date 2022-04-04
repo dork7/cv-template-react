6228fb0124423b1e1f7c54860ac96d08108c95db
dork7 <weeday425@gmail.com>
2022-04-04 14:17:20 +0500

    button floating

--- src/components/Buttons/FloatingButton.js
+++ src/components/Buttons/FloatingButton.js
@@ -18,85 +18,71 @@ const FloatingButton = () => {
 
   return (
     <>
-      <motion.div
-        style={{
-          // width: 150,
-          // height: 150,
-          position: "fixed",
-          borderRadius: "30px",
-          zIndex: 999,
-          bottom: "40px",
-          right: "40px",
-
-          // backgroundColor: "#fff",
+      <Fab
+        mainButtonStyles={{
+          color: "#FF6347",
+          backgroundColor: "#2b2b2b",
         }}
+        actionButtonStyles={{
+          color: "red",
+        }}
+        style={{
+          bottom: "10px",
+          right: "40px",
+        }}
+        icon={<AddIcon />}
+        // event={event}
+        alwaysShowTitle={true}
       >
-        <Fab
-          mainButtonStyles={{
-            color: "#FF6347",
-            backgroundColor: "#2b2b2b",
-          }}
-          actionButtonStyles={{
-            color: "red",
-          }}
-          style={{
-            bottom: "40px",
-            right: "40px",
-          }}
-          icon={<AddIcon />}
-          // event={event}
-          alwaysShowTitle={true}
-        >
-          {links.linkedin && (
-            <Action
-              text="LinkedIn"
-              style={{
-                backgroundColor: "#2b2b2b",
-              }}
-            >
-              <Link href={links.linkedin} isExternal>
-                <Icon as={FaLinkedinIn} w={6} h={6} />
-              </Link>
-            </Action>
-          )}
-          {links.github && (
-            <Action
-              text="Github"
-              style={{
-                backgroundColor: "#2b2b2b",
-              }}
-            >
-              <Link href={links.github} isExternal>
-                <Icon as={FaGithub} w={6} h={6} color="#FF6347" />
-              </Link>
-            </Action>
-          )}
-          {links.facebook && (
-            <Action
-              text="Facebook"
-              style={{
-                backgroundColor: "#2b2b2b",
-              }}
-            >
-              <Link href={links.facebook} isExternal>
-                <Icon as={FaFacebookF} w={6} h={6} color="#FF6347" />
-              </Link>{" "}
-            </Action>
-          )}
-          {links.instagram && (
-            <Action
-              text="Instagram"
-              style={{
-                backgroundColor: "#2b2b2b",
-              }}
-            >
-              <Link href={links.instagram} isExternal>
-                <Icon as={FaInstagram} w={6} h={6} color="#FF6347" />
-              </Link>{" "}
-            </Action>
-          )}
-        </Fab>
-      </motion.div>
+        {links.linkedin && (
+          <Action
+            text="LinkedIn"
+            style={{
+              backgroundColor: "#2b2b2b",
+            }}
+          >
+            <Link href={links.linkedin} isExternal>
+              <Icon as={FaLinkedinIn} w={6} h={6} />
+            </Link>
+          </Action>
+        )}
+        {links.github && (
+          <Action
+            text="Github"
+            style={{
+              backgroundColor: "#2b2b2b",
+            }}
+          >
+            <Link href={links.github} isExternal>
+              <Icon as={FaGithub} w={6} h={6} color="#FF6347" />
+            </Link>
+          </Action>
+        )}
+        {links.facebook && (
+          <Action
+            text="Facebook"
+            style={{
+              backgroundColor: "#2b2b2b",
+            }}
+          >
+            <Link href={links.facebook} isExternal>
+              <Icon as={FaFacebookF} w={6} h={6} color="#FF6347" />
+            </Link>{" "}
+          </Action>
+        )}
+        {links.instagram && (
+          <Action
+            text="Instagram"
+            style={{
+              backgroundColor: "#2b2b2b",
+            }}
+          >
+            <Link href={links.instagram} isExternal>
+              <Icon as={FaInstagram} w={6} h={6} color="#FF6347" />
+            </Link>{" "}
+          </Action>
+        )}
+      </Fab>
     </>
   );
 };
