1e34d59ef2dd503cecd7d8260a2fa99bbdf41262
dork7 <weeday425@gmail.com>
2022-06-16 13:26:07 +0500

    cv refactored

--- src/components/cv_components/hooks/useUserDataSet.js
+++
@@ -1,8 +0,0 @@
-import User from '../../../dataSet.json';
-export const useUserDataSet = () => {
-  // fetch user data from somewhere
-  // RN i ma loading it from a json file
-  const userData = User;
-
-  return userData;
-};
