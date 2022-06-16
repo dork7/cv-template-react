22c0424a59537096b2cc5f036675291ac4e96997
dork7 <weeday425@gmail.com>
2022-06-16 13:24:11 +0500

    error boundary added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { ChakraProvider } from '@chakra-ui/react';
-import { extendTheme } from '@chakra-ui/react';
-import CV from './CV';
-
-import HomePage from './pages/HomePage';
+import { ChakraProvider, extendTheme, Flex, Spinner } from '@chakra-ui/react';
+import React, { Suspense } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+const LazyLoadedCV = React.lazy(() => import('./CV'));
+const LazyLoadedHomePage = React.lazy(() => import('./pages/HomePage'));
 
 const config = {
   initialColorMode: 'dark',
   useSystemColorMode: false,
 };
 const theme = extendTheme({ config });
+const WaitingSpinner = () => {
+  return (
+    <>
+      <Flex h="100vh" justify={'center'} alignItems="center">
+        <Spinner size={'xl'} />
+      </Flex>
+    </>
+  );
+};
 
 function App() {
   return (
     <>
       <ChakraProvider theme={theme}>
-        <HomePage />
-        {/* <CV />  */}
+        <Suspense fallback={<WaitingSpinner />}>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<LazyLoadedHomePage />} />
+
+              <Route path="/cv/:userId" element={<LazyLoadedCV />} />
+            </Routes>
+          </BrowserRouter>
+        </Suspense>
       </ChakraProvider>
     </>
   );
--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -2,6 +2,8 @@ import { Box, Button } from '@chakra-ui/react';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import * as Scroll from 'react-scroll';
+import { ErrorBoundary } from 'react-error-boundary';
+import ErrorFallback from '../../components/ErrorBoundary';
 import {
   Link,
   Element,
@@ -65,53 +67,59 @@ const HomePage = () => {
   return (
     <>
       <NavBar {...{ navChange }} />
-      <Box>
-        {/* <Button onClick={scrollTo}>go to 3</Button>
+      <ErrorBoundary
+        FallbackComponent={ErrorFallback}
+        onReset={() => {
+          // reset the state of your app so the error doesn't happen again
+        }}
+      >
+        <Box>
+          {/* <Button onClick={scrollTo}>go to 3</Button>
         <Button onClick={scrollToWithContainer}>multi</Button> */}
-        <Element name="home" className="element">
-          <Box h="100vh" bg="gray">
-            <Splash />
-          </Box>
-        </Element>
-
-        <Element name="generate-cv" className="element">
-          <Box h="100vh">generate-cv</Box>
-        </Element>
-        <Element name="about" className="element">
-          <Box h="100vh">About</Box>
-        </Element>
-        <Element
-          className="element"
-          id="scroll-container"
-          style={{
-            position: 'relative',
-            height: '200px',
-            overflow: 'scroll',
-            scrollbarWidth: 'none',
-            marginBottom: '100px',
-          }}
-        >
-          <Element
-            name="scroll-container-first-element"
-            style={{
-              marginBottom: '200px',
-            }}
-          >
-            first element inside container
+          <Element name="home" className="element">
+            <Box h="100vh" bg="gray">
+              <Splash />
+            </Box>
           </Element>
 
+          <Element name="generate-cv" className="element">
+            <Box h="100vh">generate-cv</Box>
+          </Element>
+          <Element name="about" className="element">
+            <Box h="100vh">About</Box>
+          </Element>
           <Element
-            name="scroll-container-second-element"
+            className="element"
+            id="scroll-container"
             style={{
-              marginBottom: '200px',
+              position: 'relative',
+              height: '200px',
+              overflow: 'scroll',
+              scrollbarWidth: 'none',
+              marginBottom: '100px',
             }}
           >
-            second element inside container
-          </Element>
-        </Element>
-        <Button onClick={scrollToTop}>top</Button>
-      </Box>
+            <Element
+              name="scroll-container-first-element"
+              style={{
+                marginBottom: '200px',
+              }}
+            >
+              first element inside container
+            </Element>
 
+            <Element
+              name="scroll-container-second-element"
+              style={{
+                marginBottom: '200px',
+              }}
+            >
+              second element inside container
+            </Element>
+          </Element>
+          <Button onClick={scrollToTop}>top</Button>
+        </Box>
+      </ErrorBoundary>
       <Footer />
     </>
   );
